2ab964a9af3c09d4ad7b6069b47fac5c27942cbf
Adam Curtis <acurtis@spsu.edu>
2014-09-03 14:45:27 -0400

    on second thought the skipSpace should go on the end of ircLine to chop off the trailing line ending from the parse leftovers

--- src/Network/IRC/ByteString/Parser.hs
+++ src/Network/IRC/ByteString/Parser.hs
@@ -136,5 +136,5 @@ mess = spaces >> fromMaybe "" <$>
        optional (char ':' >> Word8.takeWhile (not . isEndOfLine))
        <?> "message body"
 
-ircLine = skipSpace >> IRCMsg <$> optional prefix <*> command <*> params <*> mess
-          <?> "IRC line"
+ircLine = IRCMsg <$> optional prefix <*> command <*> params <*> mess
+          <?> "IRC line" >> skipSpace
